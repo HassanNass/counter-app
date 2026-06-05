2df6ffcdea1771f9731343dade9d66c78e63470f
Hassan Nass <hassannass613@gmail.com>
2026-06-05 14:22:20 +0300

    Add counter logic - increment, decrement, reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+    <div className="container">
       <h1>Counter App</h1>
+      <p>{count}</p>
+      <button className="increment" onClick={() => setCount(count + 1)}>Increment</button>
+      <button className="decrement" onClick={() => setCount(count - 1)}>Decrement</button>
+      <button className="reset" onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
